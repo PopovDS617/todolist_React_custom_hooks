838c4492bf915e018e47535f520f51630d9ae5bd
PopovDS617 <popovds.bm@gmail.com>
2022-07-17 18:27:59 +0300

    custom hook created

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
 import React, { useEffect, useState } from "react";
+import useHttp from "./hooks/use-http";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-40e08-default-rtdb.europe-west1.firebasedatabase.app/tasks.json"
-      );
+  const transformtasks = (tasksObj) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
     }
-    setIsLoading(false);
   };
+
+  useHttp({
+    url: "https://react-http-40e08-default-rtdb.europe-west1.firebasedatabase.app/tasks",
+  });
 
   useEffect(() => {
     fetchTasks();
